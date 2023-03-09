4318e9bd6f5e1fbdf7fd389b45f0d8d2ae26c5e4
sadia901108 <68383808+sadia901108@users.noreply.github.com>
2023-03-09 10:19:39 +0600

    Add files via upload

---
+++ LoopDemo.java
@@ -0,0 +1,25 @@
+
+package basic.anisulvideo2;
+
+public class LoopDemo {
+    public static void main(String[] args)
+    {
+       /* for(int i=1;i<=10;i++)
+        {
+            System.out.println(i+" Bangladesh");
+        }
+        System.out.println(" ");
+        int i=2;
+        while(i<=20)
+        {
+            System.out.println(i+" Bangladesh");
+            i+=2;
+        } */
+        int sum=0;
+        for(int i=1;i<=10;i++)
+        {
+            sum=sum+i;
+        }
+        System.out.print("The sum of 1 to 10 is: "+sum);
+    }
+}
